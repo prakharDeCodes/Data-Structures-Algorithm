a82e17457367f3134ebc12f0915febc43b27cda9
Prakhar Yadav <yadav.prakhar450@gmail.com>
2026-07-05 12:55:10 +0530

    Time: 15 ms (12.00%) | Memory: 145 MB (7.04%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,50 +1,50 @@
-// class Solution {
-// public:
-//     vector<int> rearrangeArray(vector<int>& nums) {
-//         vector<int> pos;
-//         vector<int> neg;
-
-//         for (int x : nums) {
-//             if (x > 0)
-//                 pos.push_back(x);
-//             else
-//                 neg.push_back(x);
-//         }
-
-//         vector<int> ans;
-
-//         for (int i = 0; i < pos.size(); i++) {
-//             ans.push_back(pos[i]);
-//             ans.push_back(neg[i]);
-//         }
-
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n=nums.size();
+        vector<int> pos;
+        vector<int> neg;
 
-        vector<int> ans(n);
+        for (int x : nums) {
+            if (x > 0)
+                pos.push_back(x);
+            else
+                neg.push_back(x);
+        }
 
-        int pos=0;
-        int neg=1;
+        vector<int> ans;
 
-        for (int i=0; i<n;i++) {
-
-            if (nums[i]>0) {
-                ans[pos]=nums[i];
-                pos = pos + 2;
-            }
-            else {
-                ans[neg] = nums[i];
-                neg = neg + 2;
-            }
-
+        for (int i = 0; i < pos.size(); i++) {
+            ans.push_back(pos[i]);
+            ans.push_back(neg[i]);
         }
 
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         int n=nums.size();
+
+//         vector<int> ans(n);
+
+//         int pos=0;
+//         int neg=1;
+
+//         for (int i=0; i<n;i++) {
+
+//             if (nums[i]>0) {
+//                 ans[pos]=nums[i];
+//                 pos = pos + 2;
+//             }
+//             else {
+//                 ans[neg] = nums[i];
+//                 neg = neg + 2;
+//             }
+
+//         }
+
+//         return ans;
+//     }
+// };
